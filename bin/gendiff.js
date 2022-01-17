5942c4cafeee0b4af5524155358114b9dcb40aed
Тимур Хабибов <Tim-him@mail.ru>
2022-01-17 12:04:42 +0500

    add new options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,7 +4,10 @@ const gendiffHelp = new Command();
 
 gendiffHelp
   .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .option('-V, --version', 'output the version number')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format');
 
 gendiffHelp.parse();
