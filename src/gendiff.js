af0aec700b318a293180481d9f2020cebda51287
Тимур Хабибов <Tim-him@mail.ru>
2022-02-10 21:51:36 +0500

    final fix

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,16 +1,11 @@
-import fs from 'fs';
-import path from 'path';
 import genDiff from './filediff.js';
 import parser from './fileParser.js';
-import render from './formatters/index.js';
 
 const genDifference = (path1, path2, formatName = 'stylish') => {
-  const filePath1 = path.resolve('__fixtures__', path1);
-  const fileObj1 = parser(fs.readFileSync(filePath1, 'utf-8'), path1);
-  const filePath2 = path.resolve('__fixtures__', path2);
-  const fileObj2 = parser(fs.readFileSync(filePath2, 'utf-8'), path2);
-  const result = genDiff(fileObj1, fileObj2);
-  return render(result, formatName);
+  console.log(path1, path2);
+  const fileObj1 = parser(path1);
+  const fileObj2 = parser(path2);
+  return genDiff(fileObj1, fileObj2, 0, formatName);
 };
 
 export default genDifference;
